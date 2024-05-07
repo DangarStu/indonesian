1162832d60cc1f3db39bd5ed94362e92b86b7861
stuartatfame <139832550+stuartatfame@users.noreply.github.com>
2024-05-07 14:13:08 +1000

    New versions of generated crosswords, food is done.

--- crosswords/build.sh
+++ crosswords/build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 export build_count=100000
-./generate.py --builds $build_count --input-file ../dict/social.tsv --output-file social-1.xd
-rm social-1.xd-guesses.json1
+#./generate.py --builds $build_count --input-file ../dict/social.tsv --output-file social-1.xd
+#rm social-1.xd-guesses.json1
 ./generate.py --builds $build_count --input-file ../dict/business.tsv --output-file business-1.xd
 rm business-1.xd-guesses.json1
-./generate.py --builds $build_count --input-file ../dict/food.tsv --output-file food-1.xd
-rm food-1.xd-guesses.json1
-./generate.py --builds $build_count --input-file ../dict/weather.tsv --output-file weather-1.xd
-rm weather-1.xd-weather.json1
+#./generate.py --builds $build_count --input-file ../dict/food.tsv --output-file food-1.xd
+#rm food-1.xd-guesses.json1
+#./generate.py --builds $build_count --input-file ../dict/weather.tsv --output-file weather-1.xd
+#rm weather-1.xd-weather.json1
